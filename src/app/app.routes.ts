eb940ee936ff418774c57170d350cbc36a02c9e0
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-11-28 19:43:04 +0100

    feat(404): create 404 not found component & add to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { PostsComponent } from './posts/posts.component';
 import { NewPostComponent } from './new-post/new-post.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    {path: '', component: PostsComponent},
-    {path: 'create-post', component: NewPostComponent}
+    {path: '', title: 'Angular Poster', component: PostsComponent},
+    {path: 'create-post', title: 'Create post', component: NewPostComponent},
+    {path: '**', title: '404', component: PageNotFoundComponent}
 ];
