6eda08c4ebbaa96ba0c1a096e9793ac9058e71f6
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-11-28 15:20:53 +0100

    feat(header): create main header component

---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-header.component.html',
+  styleUrl: './main-header.component.css'
+})
+export class MainHeaderComponent {
+
+}
