8e741328a2a5552ca00d718d89bfda9459bc7e95
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-11-28 19:44:23 +0100

    feat(form): add form handling features

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ModalComponent } from "../modal/modal.component";
+import { ReactiveFormsModule, FormGroup, FormControl, Validators} from '@angular/forms';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
-    selector: 'app-new-post',
-    standalone: true,
-    templateUrl: './new-post.component.html',
-    styleUrl: './new-post.component.css',
-    imports: [CommonModule, ModalComponent]
+  selector: 'app-new-post',
+  standalone: true,
+  templateUrl: './new-post.component.html',
+  styleUrl: './new-post.component.css',
+  imports: [CommonModule, ModalComponent, ReactiveFormsModule],
 })
 export class NewPostComponent {
+  newPostForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    text: new FormControl('', Validators.required),
+  })
 
+  handleSumbit(){
+    alert(
+      'A new post has been added! ' + this.newPostForm.value.name + ' | ' + this.newPostForm.value.text
+    )
+  }
 }
